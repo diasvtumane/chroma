8e4d54887a3d7938ec20b446f66e4fb106af81bb
Bradlng <140041935+Bradlng@users.noreply.github.com>
2023-07-28 23:41:11 -0400

    Add files via upload

--- Chroma/src/appStore.java
+++ Chroma/src/appStore.java
@@ -2,22 +2,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The appStore class maintains a list of App.
+ */
 public class appStore {
+    // A list of apps maintained by the appStore
     private List<App> appList;
 
-
+    /**
+     * Constructor
+     */
     public appStore() {
         this.appList = new ArrayList<>();
     }
 
+    /**
+     * Returns the list of apps.
+     * @return A list of App objects.
+     */
+    public List<App> getAppList() {
+        return this.appList;
+    }
+
+    /**
+     * Sets a new list for apps.
+     * @param appList A list of new App objects.
+     */
+    public void setAppList(List<App> appList) {
+        this.appList = appList;
+    }
+
+    /**
+     * Adds a new app to the list.
+     * @param app An App object to be added to the list.
+     */
     public void addApp(App app) {
         this.appList.add(app);
     }
 
+    /**
+     * Removes an app from the list.
+     * @param app An App object to be removed from the list.
+     */
     public void removeApp(App app) {
         this.appList.remove(app);
     }
-
-    
-    
 }
